4e50fe03bc9bb4e191dc8442a453da771fc1b0ec
emichaelbernardo <49082717+emichaelbernardo@users.noreply.github.com>
2022-02-09 21:12:29 -0500

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -8,6 +8,30 @@ df = pd.DataFrame({
     'second column': [10, 20, 30, 40]
     })
 
+data_url = '
+
+#DATE_COLUMN = 'date/time'
+DATA_URL = ('https://storage.cloud.google.com/guitars/data/all_used_guitars.csv')
+
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    #data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache)")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+    
+
+
+
 option = st.selectbox(
     'What brand is your guitar?',
      df['Brand'])
